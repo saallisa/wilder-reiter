ff0744bf59a1f8815e03ab2a884d72a9472cd601
Lisa Saalfrank <lisa.saalfrank@web.de>
2024-05-25 23:38:24 +0200

    Programmiere einen sehr simplen Router

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# wilder-reiter
- Wilder Reiter Neuentwicklung
+# Wilder Reiter
+
+Im Jahr 2018 wurde das beliebte Pferde-Browserspiel Wilder Reiter
+eingestellt. Dieses Projekt soll es wieder zum Leben erwecken.
+
---
+++ web/index.php
@@ -0,0 +1,54 @@
+<?php
+
+declare(strict_types=1);
+
+// ----------------------------------------------------------------------
+// WILDER REITER - HAUPTDATEI
+// ----------------------------------------------------------------------
+
+// Definiere, welche Datei bei welcher Server-Anfrage eingebunden wird.
+$routes = [
+    'GET' => [
+        // Startseite
+        '/' => 'home.php',
+        // Rechtliche Seiten
+        '/imprint' => 'imprint.php',
+        '/privacy' => 'privacy.php',
+        '/rules' => 'rules.php',
+        // Benutzersystem
+        '/logout' => 'logout.php'
+    ],
+    'POST' => [
+        // Benutzersystem
+        '/login' => 'login.php',
+        '/register' => 'register.php',
+        '/verify-email' => 'verify-email.php'
+    ]
+];
+
+// Anfrage-Methode
+$requestMethod = $_SERVER['REQUEST_METHOD'];
+$requestMethod = trim($requestMethod);
+$requestMethod = strtoupper($requestMethod);
+
+// Anfrage-Pfad
+$requestUri = $_SERVER['REQUEST_URI'];
+$requestPath = strtok($requestUri, '?');
+$requestPath = '/' . trim($requestPath, '/');
+
+// Finde heraus, ob eine Datei dieser Anfrage zugewiesen wurde
+if (isset($routes[$requestMethod][$requestPath])) {
+    // Falls ja, baue den Pfad zur Datei
+    $file = __DIR__ . '/../pages/' . $routes[$requestMethod][$requestPath];
+
+    // Versuche die Datei einzubinden
+    if (file_exists($file)) {
+        include $file;
+    } else {
+        // Wenn keine Datei existiert, zeige einen Fehler an.
+        include __DIR__ . '/../pages/server-error.php';
+    }
+} else {
+    // Falls keine Datei der Anfrage zugewiesen wurde, zeige einen Fehler an.
+    include __DIR__ . '/../pages/not-found.php';
+}
